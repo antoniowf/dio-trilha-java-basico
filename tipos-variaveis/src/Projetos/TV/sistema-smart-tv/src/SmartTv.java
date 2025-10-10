cf88cb3aee504553a86293c8530d8f2219c29411
Antonio W. de Freitas <antoniowdefreitas@outlook.com>
2025-10-10 14:02:56 -0300

    métodos - 3

--- tipos-variaveis/src/Projetos/TV/sistema-smart-tv/src/SmartTv.java
+++ tipos-variaveis/src/Projetos/TV/sistema-smart-tv/src/SmartTv.java
@@ -10,4 +10,30 @@ public class SmartTv {
     public void desligar(){
         ligada = false;
     }
+
+    public void aumentarVolume(){
+        System.out.println("Aumentando o volume para: " + volume);
+        volume++;
+    }
+
+    public void diminuirVolume(){
+        System.out.println("Diminuindo o volume para: " + volume);
+        volume--;
+    }
+
+    public void mudarCanal(int novoCanal){
+        System.out.println("Mudando para canal: " + novoCanal + " channel");
+        canal = novoCanal;
+    }
+
+    public void canalUp(){
+        System.out.println("Mudando para canal: " + (canal+1) + " channel");
+        canal++;
+    }
+
+    public void canalDown(){
+        System.out.println("Mudando para canal: " + (canal-1) + " channel");
+        canal--;
+    }
+
 }
--- tipos-variaveis/src/Projetos/TV/sistema-smart-tv/src/Usuario.java
+++ tipos-variaveis/src/Projetos/TV/sistema-smart-tv/src/Usuario.java
@@ -6,8 +6,32 @@ public class Usuario {
         System.out.println("Tv ligada? " + smartTv.ligada);
         System.out.println("Canal atual: " + smartTv.canal);
         System.out.println("Volume atual: " + smartTv.volume);
-
+        
         smartTv.ligar ();
-        System.out.println("Tv ligada? " + smartTv.ligada);
+        System.out.println("Novo status Tv ligada? " + smartTv.ligada);
+
+        //smartTv.desligar();
+        //System.out.println("Novo status Tv ligada? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(15); //mudança direta para o canal 15 
+        
+        smartTv.canalUp(); 
+        smartTv.canalUp();
+        smartTv.canalUp();
+        smartTv.canalUp();
+        smartTv.canalUp();
+        smartTv.canalUp();
+        smartTv.canalDown();
+        smartTv.canalDown();
+        smartTv.canalDown();
+        smartTv.canalDown();
+        smartTv.canalDown();
+        smartTv.canalDown();
+
     }
 }
