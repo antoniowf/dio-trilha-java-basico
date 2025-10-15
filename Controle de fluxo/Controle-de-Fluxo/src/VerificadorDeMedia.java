2363316c66854fa9232d1256da7f98329163063a
Antonio Wenceslau de Freitas <antoniowdefreitas@outlook.com>
2025-10-15 20:28:28 -0300

    Média de recuperação

--- Controle de fluxo/Controle-de-Fluxo/src/VerificadorDeMedia.java
+++ Controle de fluxo/Controle-de-Fluxo/src/VerificadorDeMedia.java
@@ -18,9 +18,15 @@ public class VerificadorDeMedia {
         double notageral = (notaMatematica + notaPortugues + notaQuimica) / 3;
         double media = 7;
         
-        if (media <= notageral) {
+        if (notageral >= media) {
             System.out.println("Você foi aprovado, sua média final é de " + notageral);
-        } else {
+        } 
+        
+        else if (notageral < media && notageral >= 5) {
+            System.out.println("Você está de recuperação, sua média foi " + notageral);
+        }
+        
+        else {
             System.out.println("Você foi reprovado, sua média foi de " + notageral);
         } 
     }
